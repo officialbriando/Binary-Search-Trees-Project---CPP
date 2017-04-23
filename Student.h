657be91610bf2e9191c94e05b947f935328b61c3
Brian Do <officialbriando@gmail.com>
2017-04-23 06:08:14 -0700

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,31 +1,35 @@
+#include <string>
+
 using namespace std;
-class Student{
-private:
-	int id;
-	string name;
-	string level;
-	string major;
-	int advisor;
-	
-public:
-	Student();
-	~Student();
-	
-	void initStud(int k);
-	void setId(int myId);
-	void setName(string myName);
-	void setLevel(string myLevel);
-	void setMajor(string myMajor);
-	void setAdvisor(int myAdv);
-	
-	void printRecord();
-	int getId();
-	string getName();
-	string getLevel();
-	string getMajor();
-	int getAdvisor();
-	
-	bool isEqual(Student n);
-	bool isLesser(Student n);
-	void serialize(ofstream& file);
+
+class Student
+{
+	private:
+		int id;
+		string name;
+		string level;
+		string major;
+		int advisor;
+		
+	public:
+		Student();
+		~Student();
+		
+		void initStud(int k);
+		void setId(int myId);
+		void setName(string myName);
+		void setLevel(string myLevel);
+		void setMajor(string myMajor);
+		void setAdvisor(int myAdv);
+		
+		void printRecord();
+		int getId();
+		string getName();
+		string getLevel();
+		string getMajor();
+		int getAdvisor();
+		
+		bool isEqual(Student n);
+		bool isLesser(Student n);
+		void serialize(ofstream& file);
 };
