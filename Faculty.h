8cc08d04331c88f5ff574b1ac264c9409dc04d2d
Brian Do <officialbriando@gmail.com>
2017-04-23 06:06:38 -0700

    Update Faculty.h

--- Faculty.h
+++ Faculty.h
@@ -1,33 +1,39 @@
+#include <string>
+
 using namespace std;
-class Faculty{
-private:
-	int id;
-	string name;
-	string level;
-	string dept;
-	int studentNum;
-	int *students;
-	
-public:
-	Faculty();
-	~Faculty();
-	
-	void initFac(int k);
-	void setId(int myId);
-	void setName(string myName);
-	void setLevel(string myLevel);
-	void setDept(string myDept);
-	
-	void printRecord();
-	int getId();
-	string getName();
-	string getLevel();
-	string getDept();
-	int getStudent(int n);
-	
-	void addStudent(int id);
-	void removeStudent(int id);
-	bool isEqual(Faculty n);
-	bool isLesser(Faculty n);
-	void serialize(ofstream& file);
+
+class Faculty
+{
+	private:
+		int id;
+		string name;
+		string level;
+		string dept;
+		int studentNum;
+		int *students;
+		
+	public:
+		Faculty();
+		~Faculty();
+		
+		void initFac(int k);
+		void setId(int myId);
+		void setName(string myName);
+		void setLevel(string myLevel);
+		void setDept(string myDept);
+		
+		void printRecord();
+		int getID();
+		string getName();
+		string getLevel();
+		string getDept();
+		int* getAllStudents();
+		int getStudentNum();
+		int getStudent(int n);
+		
+		void addStudent(int id);
+		void removeStudent(int id);
+		bool isEqual(Faculty n);
+		bool isLesser(Faculty n);
+		void serialize(ofstream& file);
 };
