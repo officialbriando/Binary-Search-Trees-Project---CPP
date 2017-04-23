2399836b744c37a258b5f322b3cbcfde74d54530
Brian Do <officialbriando@gmail.com>
2017-04-23 06:08:44 -0700

    Update StudentTree.h

--- StudentTree.h
+++ StudentTree.h
@@ -1,20 +1,31 @@
+#include <string>
 #include <iostream>
+#include <fstream>
 #include "BSTree.h"
 #include "Student.h"
+
 using namespace std;
 
-class StudentTree{
-private:
-	BSTree<Student> myTree;	
-public:
-	StudentTree();
-	~StudentTree();
-	
-	void addStudent(int k);
-	void deleteStudent(int k);
-	Student getStudent(int k);
-	
-	void serializeStudent();
-	void treeTraversal(TreeNode<Student> *n, ofstream& file);
-	void deserializeStudent(string file);
+class StudentTree
+{
+	private:
+		BSTree<Student> myTree;	
+	public:
+		StudentTree();
+		~StudentTree();
+
+		TreeNode<Student>* getRoot();
+		
+		void addStudent(int k);
+		void deleteStudent(int k);
+		void changeAdvisor(int k, int j);
+		Student getStudent(int k);
+		int getStudentAdvisor(int k);
+		void printStudent(int k);
+		
+		void serializeStudent();
+		void treeTraversal(TreeNode<Student> *n, ofstream& file);
+		void deserializeStudent(string file);
+
+		void printTree(TreeNode<Student> *n);
 };
