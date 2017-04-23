c0bb88be74f1935dbd56d49382f207b5bd058154
mgotao <m.gotao@yahoo.com>
2017-04-22 21:51:47 -0700

    Fixed to work with FacultyTree

--- Faculty.cpp
+++ Faculty.cpp
@@ -13,11 +13,11 @@ Faculty::~Faculty(){
 	
 }
 
-void Faculty::initFac(){
-	cout << "ID: ";		cin >> id;
-	cout << "Name: ";	cin >> name;
-	cout << "Level: ";	cin >> level;
-	cout << "Department: ";	cin >> dept;	
+void Faculty::initFac(int k){
+	id = k;
+	cout << "Name: ";	getline(cin, name);
+	cout << "Level: ";	cin >> level;	cin.get();
+	cout << "Department: ";	getline(cin, dept);
 }
 
 void Faculty::setId(int myId){
@@ -34,6 +34,16 @@ void Faculty::setLevel(string myLevel){
 
 void Faculty::setDept(string myDept){
 	dept = myDept;
+}
+
+void Faculty::printRecord(){
+	cout << "Faculty ID: " << getId() << endl;
+	cout << "Name: " << getName() << endl;
+	cout << "Current Level: " << getLevel() << endl;
+	cout << "Department: " << getDept() << endl;
+	for(int i = 0; i < studentNum; ++i){
+		cout << "Advisee " << i+1 << ": " << getStudent(i) << endl;
+	}
 }
 
 int Faculty::getId(){
@@ -59,10 +69,10 @@ int Faculty::getStudent(int n){
 void Faculty::addStudent(int id){
 	if(studentNum >=5) cout << "Cannot hold anymore students.\n";
 	else{
+		studentNum++;
 		for(int i=0; i < studentNum; ++i){
 			if(students[i] == 0) students[i] = id;
 		}
-		studentNum++;
 	}
 }
 
