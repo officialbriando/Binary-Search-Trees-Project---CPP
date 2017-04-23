971cadd0cb8e97d4930b24ca4c884c33d9c04680
Brian Do <officialbriando@gmail.com>
2017-04-23 06:07:12 -0700

    Update FacultyTree.h

--- FacultyTree.h
+++ FacultyTree.h
@@ -1,20 +1,32 @@
+#include <string>
 #include <iostream>
 #include "BSTree.h"
 #include "Faculty.h"
+
 using namespace std;
 
-class FacultyTree{
-private:
-	BSTree<Faculty> myTree;	
-public:
-	FacultyTree();
-	~FacultyTree();
-	
-	void addFaculty(int k);
-	void deleteFaculty(int k);
-	Faculty getFaculty(int k);
-	
-	void serializeFaculty();
-	void treeTraversal(TreeNode<Faculty> *n, ofstream& file);
-	void deserializeFaculty(string file);
+class FacultyTree
+{
+	private:
+		BSTree<Faculty> myTree;	
+	public:
+		FacultyTree();
+		~FacultyTree();
+
+		TreeNode<Faculty>* getRoot();
+
+		void addFaculty(int k);
+		void deleteFaculty(int k);
+		Faculty getFaculty(int k);
+		void printFaculty(int k);
+		int* getFacultyAdvisees(int k);
+		int getNumAdvisees(int k);
+		void addAdvisee(int k, int j);
+		void removeAdvisee(int k, int j);
+		
+		void serializeFaculty();
+		void treeTraversal(TreeNode<Faculty> *n, ofstream& file);
+		void deserializeFaculty(string file);
+
+		void printTree(TreeNode<Faculty> *n);
 };
