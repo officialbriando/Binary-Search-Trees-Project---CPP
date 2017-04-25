b06c47d326a86c050a608b2df17431e8cafcb813
mgotao <m.gotao@yahoo.com>
2017-04-25 16:29:40 -0700

    Added GPA members

--- Student.h
+++ Student.h
@@ -9,17 +9,19 @@ class Student
 		string name;
 		string level;
 		string major;
+		double gpa;
 		int advisorID;
 		
 	public:
 		Student();
-		Student(int myID, string myName, string myLevel, string myMajor, int myAdv);
+		Student(int myID, string myName, string myLevel, string myMajor, double myGpa, int myAdv);
 		~Student();
 		
 		void setID(int myID);
 		void setName(string myName);
 		void setLevel(string myLevel);
 		void setMajor(string myMajor);
+		void setGpa(double myGpa);
 		void setAdvisor(int myAdv);
 		
 		void printRecord();
@@ -27,6 +29,7 @@ class Student
 		string getName();
 		string getLevel();
 		string getMajor();
+		double getGpa();
 		int getAdvisor();
 		
 		bool isEqual(Student n);
